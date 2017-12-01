186911eb4f6b194fed062479dd57e85402f5a3fb
Bhavya Jain <contact.jain.bhavya@gmail.com>
2017-12-01 01:02:44 +0100

    Added Test_Code
    Write custom tests here. Also this has to be run to start servers.

---
+++ src/Test_Code.java
@@ -0,0 +1,22 @@
+import java.rmi.RemoteException;
+import java.util.*;
+
+
+public class Test_Code {
+	
+	public void main(String[] args) {
+		
+		try {
+			
+			Manage_Servers ms = new Manage_Servers();
+			ms.startServers();
+			
+		}	catch (Exception e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+	}
+
+}
